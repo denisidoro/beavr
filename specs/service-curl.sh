2a1281708bd2166c4b4ecf185465588ec51f8a91
Denis Isidoro <denis_isidoro@live.com>
2019-04-17 11:00:34 -0300

    Rename some folders

--- specs/service-curl.sh
+++ specs/service-curl.sh
@@ -6,7 +6,7 @@ beavr::help() {
   echo "Command for calling an arbitrary URL from a service
 
 Usage:
-  curl [options] <http-method> <prototype> <service> <path>
+  service-curl [options] <http-method> <prototype> <service> <path>
   
 Options:
   -e --env <env>        Infrastructure environment
@@ -17,7 +17,7 @@ beavr::suggestion() {
   case "$1" in 
     "http-method") echo "GET POST PUT PATCH" | tr ' ' '\n';;
     "prototype") echo "s0 s1 s2 s3 global" | tr ' ' '\n';;
-    "env") echo "staging prod" | tr ' ' '\n';;
+    "--env") echo "staging prod" | tr ' ' '\n';;
     "--format") echo "xml json yaml edn transit" | tr ' ' '\n';;
     "service") work services;;
     "path") work routes "$service" | column -ts ',';;
---
+++ src/quark/README.md
@@ -0,0 +1,3 @@
+# quark ß
+
+Everything in this module should be moved to [denisidoro/quark](https://github.com/denisidoro/quark) after reaching stability.
